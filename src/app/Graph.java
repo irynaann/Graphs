95400c69c3218ddaba199a22333f08f6d6dd24af
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2025-06-01 16:06:11 +0200

    Add method addEdge and removeVertex

--- src/app/Graph.java
+++ src/app/Graph.java
@@ -7,12 +7,31 @@ import java.util.Map;
 
 public class Graph {
 
-    Map<Integer, List<Integer>> graph = new HashMap <>();
+    Map<Integer, List<Integer>> graph = new HashMap<>();
 
-    void addVertex(int vertex){
-        if(!graph.containsKey(vertex)){
-        graph.put(vertex, new ArrayList<>());
+    void addVertex(int vertex) {
+        if (!graph.containsKey(vertex)) {
+            graph.put(vertex, new ArrayList<>());
+        }
     }
 
-}
+    void addEdge(int source, int destination) {
+        addVertex(source);
+        addVertex(destination);
+        graph.get(source).add(destination);
+    }
+
+
+    void removeVertex(int vertex) {
+        if (graph.containsKey(vertex)) {
+            graph.remove(vertex);
+
+            for (List<Integer> n : graph.values()) {
+                n.remove(Integer.valueOf(3));
+            }
+
+        }
+
+    }
+
 }
